2f484932f19e3d0beb047263c488c72660487e73
Luis Davis <ldavis@intellisys.com.do>
2016-02-26 23:42:28 -0400

    module

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 
 
 
-(function(){
 
-var MenuController = function ($scope){
+// (function(){
+
+var app = angular.module("githubView", [])
+
+var MenuController = function ($scope, $http){
+
+	var whenError = function(reason){
+		$scope.error = "there nothig todo error we cant get any data"
+	};
+
+	$http.get("http://api.github.com/users/robconery").then(function(response){
+		$scope.github = response.data;
+	}, whenError);
+
+	// var promise = $http.get("http://google.com")
+	// promise.then(function(response){
+	// 	$scope.user = response.data;
+	// });
+
+	// $http.get("http://google.com").then(function(response){
+	// 	$scope.user = response.data;
+	// });
+
+	$scope.yourname = {
+		name: "javier",
+		lastName: "Allen",
+		imageSrc: "localhost"
+	}
 	$scope.message = "hello";
 }
 
 
-var create = function(){
-	var task1 = function(){
-		console.log('jeje')
-	}
-	return{
-		job1: task1
-	}
-}
+// var create = function(){
+// 	var task1 = function(){
+// 		console.log('jeje')
+// 	}
+// 	return{
+// 		job1: task1
+// 	}
+// }
 
-var worker = create();
+// var worker = create();
 
-worker.job1();
-worker.job1();
+// worker.job1();
+// worker.job1();
 // worker.job2();
 
-})();
+
+app.controller("MenuController", ["$scope", "$http", MenuController]);
+// })();
 
